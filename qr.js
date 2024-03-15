81f9200f3392b096e8f27ff5e2563c50fab75dd3
safa <ahmedsafa0759@gmail.com>
2024-03-15 20:26:38 +0600

    updated more

--- qr.js
+++ qr.js
@@ -1,16 +1,17 @@
-let qrdiv = document.getElementById("qrdiv");
-    let qrimg = document.getElementById("qrimg");
-    let qrtext = document.getElementById("qrtext");
+// let qrdiv = document.getElementById("qrdiv");
+let qrimg = document.getElementById("qrimg");
+let qrtext = document.getElementById("qrtext");
 
-    function GenerateQR() {
-        if (qrtext.value.length != 0) {
-            qrimg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
-            document.getElementById("qrContent").textContent = "Generated QR code for: " + qrText.value;
-        }
-        else alert("Please enter text/url to generate QR code.");
-    }
-    function openQRWebsite() {
-        var qrLink = qrimg.src;
-        if (qrLink)
-            window.open(qrLink);
-    }
+function GenerateQR() {
+  if (qrtext.value.length != 0) {
+    qrimg.src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      qrtext.value;
+    document.getElementById("qrContent").textContent =
+      "Generated QR code for: " + qrText.value;
+  } else alert("Please enter text/url to generate QR code.");
+}
+function openQRWebsite() {
+  var qrLink = qrimg.src;
+  if (qrLink) window.open(qrLink);
+}
